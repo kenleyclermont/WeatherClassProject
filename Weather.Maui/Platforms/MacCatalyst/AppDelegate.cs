cf54290817a2641faa60b44cdf96700e7a38aeb0
Kenley Clermont <78169956+kenleyclermont@users.noreply.github.com>
2024-03-24 15:52:02 -0700

    Add files via upload

---
+++ Weather.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Weather.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
